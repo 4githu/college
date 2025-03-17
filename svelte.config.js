ac4310094da6d01c9105d36279065df9dd571541
4githu <a56780903@gmail.com>
2025-03-17 20:40:05 +0900

    충돌 해결 및 변경 사항 추가

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -14,7 +14,11 @@ const config = {
 		adapter: adapter({
 			runtime: 'nodejs18'  // Node.js 버전 명시
 		})
-	}
+	},
+	preprocess: sequence([
+		vitePreprocess(),
+		preprocessMeltUI()
+	])
 };
 
 export default config;
